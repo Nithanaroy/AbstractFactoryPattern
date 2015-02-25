8973ea8821166b6fde2dc18a3d6fa6f96c2c98dc
Nitin Pasumarthy <nithanaroy@gmail.com>
2015-02-24 22:19:12 -0700

    Cleaned the client code for one instance of Factory

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -2,10 +2,6 @@ package client;
 
 import java.util.Map;
 
-import org.json.JSONObject;
-
-import readers.IReader;
-import readers.XMLReader;
 import restaurant.FoodItem;
 import restaurant.FoodItemCategory;
 import factory.IGenerateMenu;
@@ -22,41 +18,11 @@ public class Client {
 
 		IGenerateMenu menuGenerator = new JsonHtmlAllDay();
 		FoodItem[] allItems = menuGenerator.fetchFoodItems(jsonInpFilePath);
-
-		IReader r = new XMLReader();
-		allItems = r.processDocument(xmlInpFilePath);
-
 		allItems = menuGenerator.filterItemsForCountry("GB", allItems);
 		Map<FoodItemCategory, FoodItem[]> groupedItems = menuGenerator
 				.groupItemsByRestaurantCategory(allItems);
-		 menuGenerator.printMenu(groupedItems, htmlOutFile);
-
-		// IWriter w = new TextWriter();
-		// w.createDocument(groupedItems, textOutFile);
-
-		// IWriter w = new XMLWriter();
-		// w.createDocument(groupedItems, xmlOutFile);
+		menuGenerator.printMenu(groupedItems, htmlOutFile);
 
 		System.out.println("Done");
 	}
-
-	@SuppressWarnings("unused")
-	private static void jsonToJava() {
-		String s = "{\"1\":{\"2\":{\"3\":{\"4\":[5,{\"6\":7}]}}}}";
-		JSONObject json = new JSONObject(s);
-		json.get("1");
-	}
-
-	@SuppressWarnings("unused")
-	private static void javaToJson() {
-		JSONObject obj = new JSONObject();
-
-		obj.put("name", "foo");
-		obj.put("num", new Integer(100));
-		obj.put("balance", new Double(1000.21));
-		obj.put("is_vip", new Boolean(true));
-
-		System.out.print(obj);
-	}
-
 }
